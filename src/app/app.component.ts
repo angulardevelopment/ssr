2fab2d0672c8b33d5f1a7be0ff2c68dfe095e381
Sunny Gupta <sunny.gupta@globallogic.com>
2022-07-18 19:19:21 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { OnInit, PLATFORM_ID, Inject } from '@angular/core';
+import { isPlatformServer, isPlatformBrowser } from '@angular/common';
+import { GlobalObjectService } from './global-object.service';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rendering';
+  window;
+  constructor(@Inject(PLATFORM_ID) private platformId: Object,
+  private windowRef: GlobalObjectService,) { 
+    // console.log(window.innerHeight, 'gg'); // not working
+    console.log(this.platformId,'jkhuihu');
+    this.window = windowRef.getWindow();
+  }
+  ngOnInit() {
+      if (isPlatformServer(this.platformId)) {
+          // do server side stuff
+    console.log('isPlatformServer');
+
+      }
+
+      if (isPlatformBrowser(this.platformId)) {
+                // this.windowRef.scrollTo(0, 0);
+                console.log('isPlatformBrowser');
+                
+              }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GlobalObjectService } from './global-object.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' })
   ],
-  providers: [],
+  providers: [GlobalObjectService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
